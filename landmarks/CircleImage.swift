d846f2941be4588879285c5f530fb5c425a7201c
Wunna Kyaw <wunnakyaw.developer@gmail.com>
2023-07-25 16:36:18 +0630

    Creating and Combination Views

---
+++ landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  landmarks
+//
+//  Created by GWT on 25/07/2023.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(.white, lineWidth: 4)
+            )
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
