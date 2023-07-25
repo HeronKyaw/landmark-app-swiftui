d68132361601365bd219167b5840eb9adfde16b6
Wunna Kyaw <wunnakyaw.developer@gmail.com>
2023-07-25 17:10:23 +0630

    Building Lists and Navigation Section 5 completed

---
+++ landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  landmarks
+//
+//  Created by GWT on 25/07/2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
