1883e3f7a4d0ddb49bb32b215c7920a47f6dc7d6
Wunna Kyaw <wunnakyaw.developer@gmail.com>
2023-07-25 09:03:40 +0630

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by GWT on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
